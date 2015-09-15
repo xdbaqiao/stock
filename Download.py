390ef33edf33873a176176cce7fb5b4193b933c5
xdbaqiao <yexj061761@gmail.com>
2015-09-15 02:31:15 -0400

    添加下载程序

---
+++ Download.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python2
+#coding: utf-8
+
+import re
+import string
+import urllib
+import urllib2
+import logging
+import cookielib
+import threading
+from collections import deque
+
+def set_log():
+    FMT = '%(asctime)s-%(levelname)s\t%(message)s'
+    logging.basicConfig(format=FMT, level=logging.INFO, filename='downloading.log', filemode='a')
+
+class Download:
+    def __init__(self, first_url, proxy=None):
+        self.user_agent = 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:5.0) Gecko/20100101 Firefox/5.0'
+        self.headers = {'User-Agent': self.user_agent, 'Accept-encoding':'gzip, deflate', 'Referer': first_url}
+        self.opener = urllib2.build_opener()
+        self.proxy = proxy
+
+    def add_proxy(self, url, proxy):
+        if proxy:
+            if url.startswith('https'):
+                self.opener.add_handler(urllib2.ProxyHandler({'https': proxy}))
+            else:
+                self.opener.add_handler(urllib2.ProxyHandler({'http': proxy}))
+
+    def get(self, url):
+        self.add_proxy(url, self.proxy)
+        request = urllib2.Request(url)
+        response = self.opener.open(request)
+        html = response.read()
+        return html
+
+    def post(self, url, data):
+        self.add_proxy(url, self.proxy)
+        if isinstance(data, dict):
+            data = urllib.urlencode(data)
+        request = urllib2.Request(url, data, self.headers)
+        response = self.opener.open(request)
+        html = response.read()
+        return html
