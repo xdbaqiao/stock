91c258ffbd338eae1deee94f5215b4d8dab8f664
xdbaqiao <yexj061761@gmail.com>
2015-09-25 04:21:49 -0400

    增加登录程序

---
+++ office/login.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+# coding: utf-8
+
+import re
+
+def login():
+    url = 'https://sso.guosen.com.cn/Login.aspx'
+    post_data = {}
+    post_data[] = 
+    post_data['__LASTFOCUS'] = 
+    post_data['__VIEWSTATE'] = 
+    post_data['__EVENTTARGET'] = 
+    post_data['__EVENTARGUMENT'] = 
+    post_data['__EVENTVALIDATION'] = 
+    post_data['hidReturnUrl'] = 
+    post_data['hidIASID'] = 
+    post_data['txtUserName'] = 
+    post_data['chkRememberAccount'] = 
+    post_data['txtPassword'] = 
+    post_data['chkEnableDynamicCode'] = 
+    post_data['txtDynamicPassword'] = 
+    post_data['hidSms'] = 
+    post_data['txtValidateCode'] = 
+    post_data['btnLogin.x'] = 
+    post_data['btnLogin.y'] = 
+
+
+if __name__ == '__main__':
