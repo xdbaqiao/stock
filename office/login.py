2a812c34e2619130c283aac5e0dea516f0c3ed08
xdbaqiao <yexj061761@gmail.com>
2015-09-30 05:31:43 -0400

    add comments

--- office/login.py
+++ office/login.py
@@ -8,9 +8,10 @@ sys.path.append('..')
 import statistics.download
 
 '''
+post表单中有一个字段_xml是加密过的，从最后两个值是等号猜测是base64编码，据此做一下分析：
 __xml值
 0-S-0-NPHJwYyBpZD0iZHNBbGxvY2F0ZSIgdHlwZT0iQ3VzdG9tIiBvYmplY3RjbGF6ej0ibmMuYnMuaHJzcy50YS5DbG9ja2luSFZPIiBwaT0iMSIgcHM9IjEwMDAwIiBwYz0iMSIgcHJjPSIwIiBmcz0icGtfY2xvY2tpbl9oLFBLX0RJU1RMSVNULG51bSxkYXRlLHdlZWssYW1kYXRlLHBtZGF0ZSI$2BPHBzPjxwIG5hbWU9IkNZRUFSIj4yMDE1PC9wPjxwIG5hbWU9IkNQRVJJT0QiPjA3PC9wPjwvcHM$2BPHZwcz48cCBuYW1lPSJDWUVBUiIgdHlwZT0iMCI$2BMjAxNTwvcD48cCBuYW1lPSJDTU9OVEgiIHR5cGU9IjAiPjA3PC9wPjxwIG5hbWU9Il9mdW5jb2RlIiB0eXBlPSIwIj5FMDAyMDkwMjwvcD48cCBuYW1lPSJfX3Byb2ZpbGVLZXlzIiB0eXBlPSIwIj5kdEFsbG9jYXRlJTNCMDk4ODBiOTExZTdkMGE3ZDMzZGRhMDc4NjFmYjQxYWYlM0Jmb3JtMSUzQjc1NTFjOGNiNDRlMDI1Y2E0ZDAzMDI2ZmIyZWVhNWMwPC9wPjwvdnBzPjwvcnBjPg==
-
+直接解析得到乱码：
 分割一下：
 0-S-0-N
 PHJwYyBpZD0iZHNBbGxvY2F0ZSIgdHlwZT0iQ3VzdG9tIiBvYmplY3RjbGF6ej0ibmMuYnMuaHJzcy50YS5DbG9ja2luSFZPIiBwaT0iMSIgcHM9IjEwMDAwIiBwYz0iMSIgcHJjPSIwIiBmcz0icGtfY2xvY2tpbl9oLFBLX0RJU1RMSVNULG51bSxkYXRlLHdlZWssYW1kYXRlLHBtZGF0ZSI
@@ -21,7 +22,7 @@ PHZwcz48cCBuYW1lPSJDWUVBUiIgdHlwZT0iMCI
 $2B
 MjAxNTwvcD48cCBuYW1lPSJDTU9OVEgiIHR5cGU9IjAiPjA3PC9wPjxwIG5hbWU9Il9mdW5jb2RlIiB0eXBlPSIwIj5FMDAyMDkwMjwvcD48cCBuYW1lPSJfX3Byb2ZpbGVLZXlzIiB0eXBlPSIwIj5kdEFsbG9jYXRlJTNCMDk4ODBiOTExZTdkMGE3ZDMzZGRhMDc4NjFmYjQxYWYlM0Jmb3JtMSUzQjc1NTFjOGNiNDRlMDI1Y2E0ZDAzMDI2ZmIyZWVhNWMwPC9wPjwvdnBzPjwvcnBjPg==
 
-base64解码:
+对特殊的位置进行base64解码:
 0-S-0-N
 <rpc id="dsAllocate" type="Custom" objectclazz="nc.bs.hrss.ta.ClockinHVO" pi="1" ps="10000" pc="1" prc="0" fs="pk_clockin_h,PK_DISTLIST,num,date,week,amdate,pmdate"
 $2B
@@ -31,7 +32,7 @@ vps><p name="CYEAR" type="0"
 $2B
 2015</p><p name="CMONTH" type="0">07</p><p name="_funcode" type="0">E0020902</p><p name="__profileKeys" type="0">dtAllocate%3B09880b911e7d0a7d33dda07861fb41af%3Bform1%3B7551c8cb44e025ca4d03026fb2eea5c0</p></vps></rpc>
 
-修饰一下：
+修饰一下,得到结果：
 <rpc id="dsAllocate" type="Custom" objectclazz="nc.bs.hrss.ta.ClockinHVO" pi="1" ps="10000" pc="1" prc="0">
     <fs="pk_clockin_h,PK_DISTLIST,num,date,week,amdate,pmdate">
        <ps>
@@ -45,6 +46,7 @@ $2B
            <p name="__profileKeys" type="0">dtAllocate%3B09880b911e7d0a7d33dda07861fb41af%3Bform1%3B7551c8cb44e025ca4d03026fb2eea5c0</p>
        </vps>
 </rpc>
+反推可以得到post编码数据
 '''
 
 def get_post_xml(month, year):
