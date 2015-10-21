90ca8c45ea2fa1c8c751b1eb88a7a6a5aa41e338
xdbaqiao <yexj061761@gmail.com>
2015-10-21 05:34:29 -0400

    login.py

--- weixin/login.py
+++ weixin/login.py
@@ -4,18 +4,34 @@
 import sys 
 import re
 import os
+import hashlib
 from download import download
 from common import common_re
 
 
 def login(D, username, passwd):
-    url = 'https://mp.weixin.qq.com/'
-    print username, passwd
-
+    url = 'https://mp.weixin.qq.com/cgi-bin/login'
+    post_data = {}
+    post_data['username'] = username
+    m = hashlib.md5()
+    m.update(passwd)
+    post_data['pwd'] = m.hexdigest()
+    post_data['imgcode'] = ''
+    post_data['f'] = 'json'
+    D.post(url, data=post_data)
+    url =  'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=195186981'
+    html = D.get(url)
+    if 'success">已认证' in html:
+        print 'Login succefully!'
+        return True
+    else:
+        print 'Login fail.'
+        return False
 
 
 if __name__ == '__main__':
-    D = download()
+    refer_url = 'https://mp.weixin.qq.com/'
+    D = download(first_url=refer_url, is_cookie=True)
     username = os.environ.get('WX_username')
     passwd = os.environ.get('WX_passwd')
     login(D, username, passwd)
