3d469df980456db9827d398e7dbdc6b539dc2a2c
xdbaqiao <yexj061761@gmail.com>
2015-10-21 23:18:24 -0400

    version

--- weixin/login.py
+++ weixin/login.py
@@ -4,6 +4,7 @@
 import os
 import hashlib
 from download import download
+from common import common_re
 
 def login(D, username, passwd):
     url = 'https://mp.weixin.qq.com/cgi-bin/login'
@@ -14,12 +15,13 @@ def login(D, username, passwd):
     post_data['pwd'] = m.hexdigest()
     post_data['imgcode'] = ''
     post_data['f'] = 'json'
-    D.post(url, data=post_data)
-    url =  'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=195186981'
+    m = D.post(url, data=post_data)
+    token = common_re(r'token=([^"]+)"', m)
+    url =  'https://mp.weixin.qq.com/cgi-bin/home?t=home/index&lang=zh_CN&token=%s' % token
     html = D.get(url)
     if 'success">已认证' in html:
         print 'Login succefully!'
-        return True
+        return token 
     else:
         print 'Login fail.'
         return False
