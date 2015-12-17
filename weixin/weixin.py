f33d03bd9f6b6b75da7c74d5ba65c9969a89ec07
xdbaqiao <yexj061761@gmail.com>
2015-12-17 02:23:34 -0500

    weixin bug fixs

--- weixin/weixin.py
+++ weixin/weixin.py
@@ -117,8 +117,8 @@ def calculte_time(stime, n):
     return (d - datetime.timedelta(days=7)).strftime('%Y-%m-%d')
 
 def user_scrape(D, token, stime=''):
-    FIELDS = [u'统计时间', u'新关注人数', u'取消关注人数', u'净增关注人数', u'累积关注人数', u'搜索公众号名称',\
-            u'搜索微信号', u'图文页右上角菜单', u'名片分享', u'其他']
+    FIELDS = [u'统计时间', u'新关注人数', u'取消关注人数', u'净增关注人数', u'累积关注人数', u'公众号搜索', u'二维码扫描'\
+            ,u'图文页右上角菜单', u'名片分享', u'其他']
     writer = UnicodeWriter('user_info.csv', 'gbk')
     writer.writerow(FIELDS)
     bag = {}
@@ -134,8 +134,8 @@ def user_scrape(D, token, stime=''):
     bag[u'净增关注人数'] = common_re(r'netgain_user:\s([^,]+),', m) if m else ''
     bag[u'累积关注人数'] = common_re(r'cumulate_user:\s([^,]+),', m) if m else ''
     assert bag[u'统计时间']
-    a = ['35', u'3', '43', '17', '0']
-    b = [u'搜索公众号名称', u'搜索微信号', u'图文页右上角菜单', u'名片分享', u'其他']
+    a = ['1', '30', '43', '17', '0']
+    b = [u'公众号搜索', u'二维码扫描', u'图文页右上角菜单', u'名片分享', u'其他']
     for num, i in enumerate(a):
         gain_url = 'https://mp.weixin.qq.com/misc/useranalysis?&begin_date=%s&end_date=%s&source=%s&token=%s&lang=zh_CN&f=json&ajax=1' % (bag[u'统计时间'], bag[u'统计时间'], i, token)
         gain_html = D.get(gain_url)
